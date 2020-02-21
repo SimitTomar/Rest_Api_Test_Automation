2094beb3d239e394be15b171e5377f50483203ff
SimitTomar <simittomar8@gmail.com>
2020-02-21 18:55:07 +0000

    Added Config File

--- tests_cucumber/config.js
+++ tests_cucumber/config.js
@@ -1,24 +1,23 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const argv = require('yargs').argv;
 
-// let {setDefaultTimeout} = require('cucumber');
-// setDefaultTimeout(60 * 1000);
 
-// const exec = require('child_process').exec;
-const CUCUMBER_EXECUTABLE_PATH = `./node_modules/.bin/cucumber-js `;
-const CUCUMBER_COMMAND = `tests_cucumber/features/**/*.feature`;
+const CUCUMBER_EXECUTABLE_PATH = `./node_modules/.bin/cucumber-js`;
+const CUCUMBER_COMMAND = argv.CUCUMBER_COMMAND;
+console.log('CUCUMBER_COMMAND', CUCUMBER_COMMAND);
 const COMPLETE_CLI_COMMAND = `${CUCUMBER_EXECUTABLE_PATH} ${CUCUMBER_COMMAND}`;
 
 
-async function executeCLICommand() {
+async function executeCliCommand() {
   const { stdout, stderr } = await exec(COMPLETE_CLI_COMMAND);
   console.log('stdout:', stdout);
   console.error('stderr:', stderr);
 }
-executeCLICommand();
+executeCliCommand();
 
 
 
 
-
-
+// let {setDefaultTimeout} = require('cucumber');
+// setDefaultTimeout(60 * 1000);
